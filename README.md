65ae4e8ed28d043fb19d76e1ae6634ac1cb58dc2
Guy Tavor <guy@scoompa.com>
2023-12-09 22:22:02 +0200

    added interactive mode, some coloring, updated README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # cmdgpt
- Run GPT queries from the command line
+#### Execute GPT Queries Directly from the Command Line
+
+cmdgpt offers two modes for querying:
+
+1. **Single Question Mode**: Directly ask a question in the command line like so:
+
+   ```
+   cmdgpt "What is the meaning of life?"
+   ```
+
+2. **Interactive Session Mode**: Enter a continuous query session by simply typing `cmdgpt`. Within this mode, you can ask multiple questions:
+
+   ```
+   cmdgpt
+   > What is the meaning of life?
+   ...
+   ```
--- main.py
+++ main.py
@@ -5,44 +5,62 @@ import threading
 from halo import Halo
 
 from openai import OpenAI
+from termcolor import colored
 
 
-def query_gpt4(query: str) -> str:
+def query_gpt4(messages: list) -> str:
     client = OpenAI(
         api_key=os.environ.get("OPENAI_API_KEY"),
     )
     response = client.chat.completions.create(
         model="gpt-4-1106-preview",
-        messages=[
-            {"role": "user", "content": query}
-        ]
+        messages=messages
     )
 
     return response.choices[0].message.content
 
 
-def query_in_background(query: str, response_list: list):
-    response_list.append(query_gpt4(query))
+def query_in_background(query: str, response_list: list, messages: list):
+    response_list.append(query_gpt4(messages + [{"role": "user", "content": query}]))
+
+
+def interactive_session():
+    session_history = []
+    try:
+        while True:
+            query = input("> ")
+            if query.lower() == "exit":
+                break
+
+            session_history.append({"role": "user", "content": query})
+            response = query_gpt4(session_history)
+            print(colored(response, 'green', attrs=['bold']))
+            session_history.append({"role": "assistant", "content": response})
+    except KeyboardInterrupt:
+        print("\nSession ended.")
 
 
 def main():
-    query = ' '.join(sys.argv[1:])  # Join all arguments into a single string
-    response_list = []
+    if len(sys.argv) > 1:
+        query = ' '.join(sys.argv[1:])  # Join all arguments into a single string
+        response_list = []
 
-    # Start the query in a background thread
-    thread = threading.Thread(target=query_in_background, args=(query, response_list))
-    thread.start()
+        # Start the query in a background thread
+        thread = threading.Thread(target=query_in_background, args=(query, response_list, []))
+        thread.start()
 
-    spinner = Halo(text='Processing', spinner='dots')
-    spinner.start()
+        spinner = Halo(text='Processing', spinner='dots')
+        spinner.start()
 
-    # Wait for the background thread to finish
-    thread.join()
+        # Wait for the background thread to finish
+        thread.join()
 
-    spinner.stop()
+        spinner.stop()
 
-    if response_list:
-        print(response_list[0])  # Print the response
+        if response_list:
+            print(response_list[0])  # Print the response
+    else:
+        interactive_session()
 
 
 if __name__ == "__main__":
